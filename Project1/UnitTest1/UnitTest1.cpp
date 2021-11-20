3629c5e06ebd80d1e6096ac09e17107e8d701f06
Jungsoo Kwak <wjdtn7524@gmail.com>
2021-11-20 13:41:07 +0900

    UnitTest

--- Project1/UnitTest1/UnitTest1.cpp
+++ Project1/UnitTest1/UnitTest1.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include "CppUnitTest.h"
+#include "../Project1/RightTriangle.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -11,6 +12,36 @@ namespace UnitTest1
 		
 		TEST_METHOD(TestMethod1)
 		{
+			RightTriangle a;
+			a.height = 3;
+			a.width = 4;
+
+			Assert::AreEqual(RightTriangle_area(a), 6.0);
+
+			a.height = 4;
+			a.width = 3;
+
+			Assert::AreEqual(RightTriangle_area(a), 6.0);
+
+			a.height = 5;
+			a.width = 4;
+
+			Assert::AreEqual(RightTriangle_area(a), 10.0);
+
+			a.height = 4;
+			a.width = 5;
+
+			Assert::AreEqual(RightTriangle_area(a), 10.0);
+
+			a.height = 7;
+			a.width = 5;
+
+			Assert::AreEqual(RightTriangle_area(a), 17.5);
+
+			a.height = 5;
+			a.width = 7;
+
+			Assert::AreEqual(RightTriangle_area(a), 17.5);
 		}
 	};
 }
